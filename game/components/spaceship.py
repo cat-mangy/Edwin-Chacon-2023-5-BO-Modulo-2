4ca99e63d29721712dd434b0ef6c6b11f9bc2dec
dog-mangy <136274596+dog-mangy@users.noreply.github.com>
2023-06-13 08:50:51 -0500

    tarea-1-Edwin-Chacon

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -14,13 +14,37 @@ from game.utils.constants import SPACESHIP
 class SpaceShip(Sprite):
     
     def __init__(self):
-        self.image_size = (40, 60)
+        self.image_size = (60, 60)
+        self.position = [500,250]
+        self.velocity = 15
         self.image = pygame.transform.scale(SPACESHIP, self.image_size)
         self.image_rect = self.image.get_rect()
-        self.image_rect.x = self.image_size[0]
-        self.image_rect.y = self.image_size[1]
+        self.image_rect.x = self.position[0]
+        self.image_rect.y = self.position[1]
 
     def update(self):
-        pass
-
-
+        keystate = pygame.key.get_pressed()
+        if self.position[0] <= 0:
+            self.position[0] = 1039 
+        if self.position[0] >= 1040:
+            self.position[0] = 0
+            
+        if self.position[1] <= 0:
+            self.position[1] = 539
+        if self.position[1] >= 540:
+            self.position[1] = 0
+            
+        if keystate[pygame.K_LEFT]:
+            self.position[0] -= self.velocity
+            
+        if keystate[pygame.K_RIGHT]:
+            self.position[0] += self.velocity
+            
+        if keystate[pygame.K_UP]:
+            self.position[1] -= self.velocity
+            
+        if keystate[pygame.K_DOWN]:
+            self.position[1] += self.velocity
+            
+        self.image_rect.x = self.position[0]
+        self.image_rect.y = self.position[1]
